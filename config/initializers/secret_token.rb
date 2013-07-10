9190c3d8e94ae6319d82adec48d9dc5e89640cda
alicem <alice.mottola@gmail.com>
2013-07-10 15:50:36 -0400

    Initial commit, create movie migration

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MovieApp::Application.config.secret_token = '63158d8bec9e9c200d8aea05ef0d13b7c7f6998425852a10a621d48c98e5cfb8b5380427c0b695237ee4d13e99b203706047422ef2ef1b27ddb1c01950ea5320'
